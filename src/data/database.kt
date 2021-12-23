b2b9c36486ce16057a1b26eff03128b72f6cbc1c
gyotro <giovanni.dintrono@gmail.com>
2021-12-23 19:27:43 +0100

    repo and dataModel added

---
+++ src/data/database.kt
@@ -0,0 +1,9 @@
+package data
+
+import models.Dessert
+
+val initDessert = mutableListOf(
+    Dessert("1", "tiramisu", "masca_caffe", "wwwpipiacitu"),
+    Dessert("2", "oreo", "oreo_panna", "wwworeo"),
+    Dessert("3", "apple cake", "mele", "wwwmele")
+)
