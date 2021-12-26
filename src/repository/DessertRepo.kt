0aad6573ab2a76cad96190534c43082e338de506
gyotro <giovanni.dintrono@gmail.com>
2021-12-26 08:40:16 +0100

    added Repo

---
+++ src/repository/DessertRepo.kt
@@ -0,0 +1,33 @@
+package repository
+
+import data.initDessert
+import models.Dessert
+
+class DessertRepo : RepositoryIntertface<Dessert> {
+    override fun getById(id: String): Dessert = initDessert.filter { it.id == id }.first()
+
+    override fun getAll(): List<Dessert> = initDessert
+
+    override fun deleteById(id: String): Boolean {
+        return try {
+            initDessert.removeIf { it.id == id }
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    override fun add(entry: Dessert): Dessert {
+        initDessert.add(entry)
+        return entry
+    }
+
+    override fun update(entry: Dessert): Dessert {
+            return initDessert.find { it.id == entry.id }?.apply {
+                name = entry.name
+                description = entry.description
+                url = entry.url
+            } ?: throw Exception("No Dessert with that ID is present!!")
+        }
+
+}
